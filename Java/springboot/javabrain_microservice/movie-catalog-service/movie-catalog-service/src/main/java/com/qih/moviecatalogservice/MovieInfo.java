78b62b060be2856a7754e9a8fd829dc5947c45c7
adrrrrrrrian <qihu1992@hotmail.com>
2020-05-29 09:24:29 -0400

    Implement buldhead pattern

--- Java/springboot/javabrain_microservice/movie-catalog-service/movie-catalog-service/src/main/java/com/qih/moviecatalogservice/MovieInfo.java
+++ Java/springboot/javabrain_microservice/movie-catalog-service/movie-catalog-service/src/main/java/com/qih/moviecatalogservice/MovieInfo.java
@@ -1,6 +1,7 @@
 package com.qih.moviecatalogservice;
 
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import com.qih.moviecatalogservice.model.CatalogItem;
 import com.qih.moviecatalogservice.model.Movie;
 import com.qih.moviecatalogservice.model.Rating;
@@ -13,7 +14,12 @@ public class MovieInfo {
     @Autowired
     private RestTemplate restTemplate;
 
-    @HystrixCommand(fallbackMethod = "getFallbackCatalogItem")
+    @HystrixCommand(fallbackMethod = "getFallbackCatalogItem",
+    threadPoolKey = "movieInfoPool",
+    threadPoolProperties = {
+            @HystrixProperty(name = "coreSize", value = "20"),
+            @HystrixProperty(name = "maxQueueSize", value = "10"),
+    })
     public CatalogItem getCatalogItem(Rating rating) {
         Movie movie = restTemplate.getForObject("http://movie-info-service/movies/" + rating.getMovieId(), Movie.class);
         return new CatalogItem(movie.getName(), movie.getDescription(), rating.getRating());
