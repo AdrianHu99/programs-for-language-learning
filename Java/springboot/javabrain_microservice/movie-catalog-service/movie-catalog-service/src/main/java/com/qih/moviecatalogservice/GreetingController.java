317984c5a0baa48f9f43db7c75d663c0851d9384
adrrrrrrrian <qihu1992@hotmail.com>
2020-06-05 11:34:26 -0400

    Add default value; Add list and map in properties

--- Java/springboot/javabrain_microservice/movie-catalog-service/movie-catalog-service/src/main/java/com/qih/moviecatalogservice/GreetingController.java
+++ Java/springboot/javabrain_microservice/movie-catalog-service/movie-catalog-service/src/main/java/com/qih/moviecatalogservice/GreetingController.java
@@ -5,14 +5,26 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.Map;
+
 @RestController
 public class GreetingController {
 
-    @Value("${my.greetings}")
+    @Value("${my.greetings:default value}")
     private String greeting;
+
+    @Value("${my.list.value}")
+    private List<String> list;
+
+    @Value("#{${my.db.map}}")
+    private Map<String, String> map;
+
 
     @GetMapping("/greetings")
     public String getGreetings() {
-        return greeting;
+        return greeting + list + map;
     }
+
+
 }
