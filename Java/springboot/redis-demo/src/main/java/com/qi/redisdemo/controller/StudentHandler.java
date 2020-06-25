0e45e24beee5fa359b77ecfd4ce2e54c31845ce2
adrrrrrrrian <qihu1992@hotmail.com>
2020-06-25 11:45:26 -0400

    Add tests for different value types supported by redis

--- Java/springboot/redis-demo/src/main/java/com/qi/redisdemo/controller/StudentHandler.java
+++ Java/springboot/redis-demo/src/main/java/com/qi/redisdemo/controller/StudentHandler.java
@@ -2,8 +2,14 @@ package com.qi.redisdemo.controller;
 
 import com.qi.redisdemo.entity.Student;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.HashOperations;
+import org.springframework.data.redis.core.ListOperations;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.SetOperations;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Set;
 
 @RestController
 public class StudentHandler {
@@ -25,5 +31,48 @@ public class StudentHandler {
     public boolean delete(@PathVariable("key") String key) {
         redisTemplate.delete(key);
         return redisTemplate.hasKey(key);
+    }
+
+    @GetMapping("/string")
+    public String stringTest() {
+        redisTemplate.opsForValue().set("str", "Hello World");
+        String str = (String) redisTemplate.opsForValue().get("str");
+        return str;
+    }
+
+    @GetMapping("/list")
+    public List<String> listTest() {
+        ListOperations<String, String> listOperations = redisTemplate.opsForList();
+        listOperations.leftPush("list", "Hello");
+        listOperations.leftPush("list", "World");
+        listOperations.leftPush("list", "Java");
+        listOperations.leftPush("list", "Please");
+        listOperations.leftPush("list", "Open");
+        listOperations.leftPush("list", "The");
+        listOperations.leftPush("list", "Door");
+        List<String> list = listOperations.range("list", 0,3);
+        return list;
+    }
+
+    @GetMapping("/set")
+    public Set<String> setTest() {
+        SetOperations<String, String> setOperations = redisTemplate.opsForSet();
+        setOperations.add("set", "Hello");
+        setOperations.add("set", "Hello");
+        setOperations.add("set", "World");
+        setOperations.add("set", "World");
+        setOperations.add("set", "Java");
+        setOperations.add("set", "Java");
+
+        Set<String> set = setOperations.members("set");
+        return set;
+    }
+
+    @GetMapping("/hash")
+    public void hashTest() {
+        // key -> <hashkey, value>
+        HashOperations<String, String, String> hashOperations = redisTemplate.opsForHash();
+        hashOperations.put("key", "hashKey", "value");
+        System.out.println(hashOperations.get("key", "hashKey"));
     }
 }
