0cb5fac90cc784b3de595bd31b7c9c628a899836
adrrrrrrrian <qihu1992@hotmail.com>
2020-06-29 08:12:45 -0400

    more granular permission configuration

--- Java/springboot/security/spring-security/src/main/java/com/qih/springsecurity/SecurityConfiguration.java
+++ Java/springboot/security/spring-security/src/main/java/com/qih/springsecurity/SecurityConfiguration.java
@@ -2,6 +2,7 @@ package com.qih.springsecurity;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -26,5 +27,14 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .withUser("user2")
                 .password("user2")
                 .roles("ADMIN");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/admin").hasRole("ADMIN")
+                .antMatchers("/user").hasAnyRole("ADMIN", "USER")
+                .antMatchers("/").permitAll()
+                .and().formLogin();
     }
 }
