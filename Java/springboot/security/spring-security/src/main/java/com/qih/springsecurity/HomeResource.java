ac99c9af9e097593d39a03c209ce4592a5dcf04a
adrrrrrrrian <qihu1992@hotmail.com>
2020-07-07 20:22:07 -0400

    Implement JWT authorization

--- Java/springboot/security/spring-security/src/main/java/com/qih/springsecurity/HomeResource.java
+++ Java/springboot/security/spring-security/src/main/java/com/qih/springsecurity/HomeResource.java
@@ -1,24 +1,62 @@
 package com.qih.springsecurity;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.qih.springsecurity.services.MyUserDetailsService;
+import com.qih.springsecurity.util.JwtUtil;
+import models.AuthenticationRequest;
+import models.AuthenticationResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PostAuthorize;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class HomeResource {
 
+    @Autowired
+    private AuthenticationManager authenticationManager;
 
-    @GetMapping("/")
+    @Autowired
+    private MyUserDetailsService userDetailsService;
+
+    @Autowired
+    private JwtUtil jwtTokenUtil;
+
+    @RequestMapping("/")
     public String home() {
         return ("<h1>Welcome</h1>");
     }
 
-    @GetMapping("/user")
+    @RequestMapping("/user")
     public String user() {
         return ("<h1>Welcome user</h1>");
     }
 
-    @GetMapping("/admin")
+    @RequestMapping("/admin")
     public String admin() {
         return ("<h1>Welcome admin</h1>");
+    }
+
+    @RequestMapping(value = "/authenticate", method = RequestMethod.POST)
+    public ResponseEntity<?> createAuthenticationToken(@RequestBody AuthenticationRequest authenticationRequest) throws Exception {
+        // do authentication
+        try {
+            authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(authenticationRequest.getUsername(), authenticationRequest.getPassword())
+            );
+        } catch(BadCredentialsException ex) {
+            throw new Exception("Incorrect username or password", ex);
+        }
+        // generate userDetails
+        final UserDetails userDetails = userDetailsService.loadUserByUsername(authenticationRequest.getUsername());
+        // generate JWT token
+        final String jwt = jwtTokenUtil.generateToken(userDetails);
+
+        // respond with JWT token
+        return ResponseEntity.ok(new AuthenticationResponse(jwt));
     }
 }
--- Java/springboot/security/spring-security/src/main/java/com/qih/springsecurity/SecurityConfiguration.java
+++ Java/springboot/security/spring-security/src/main/java/com/qih/springsecurity/SecurityConfiguration.java
@@ -1,40 +1,51 @@
 package com.qih.springsecurity;
 
+import com.qih.springsecurity.filters.JwtRequestFilter;
+import com.qih.springsecurity.services.MyUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
-    @Bean
-    public PasswordEncoder getPasswordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
-    }
+    @Autowired
+    private MyUserDetailsService myUserDetailsService;
+
+    @Autowired
+    private JwtRequestFilter jwtRequestFilter;
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        // Set your customerized configuration on the auth project
-        auth.inMemoryAuthentication()
-                .withUser("user1")
-                .password("user1")
-                .roles("USER")
-                .and()
-                .withUser("user2")
-                .password("user2")
-                .roles("ADMIN");
+        auth.userDetailsService(myUserDetailsService);
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .antMatchers("/admin").hasRole("ADMIN")
-                .antMatchers("/user").hasAnyRole("ADMIN", "USER")
-                .antMatchers("/").permitAll()
-                .and().formLogin();
+        http.csrf().disable()
+                .authorizeRequests().antMatchers("/authenticate").permitAll()
+                .anyRequest().authenticated()
+                .and().sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+    }
+
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
     }
 }
